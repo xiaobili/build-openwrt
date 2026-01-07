f9d8c6ff843a786647a805d192c1c0b9b1617670
Billy JR <zygboom@gmail.com>
2026-01-07 15:30:08 +0800

    Update feed sources in diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,5 +8,5 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/xiaobili/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/xiaobili/small' feeds.conf.default
